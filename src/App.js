679b23421aea45c8789752996be60fb424bb392f
Batuhan SAHIN <baturox@gmail.com>
2023-03-11 12:59:08 +0300

    show error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import getGyroscope from 'reactjs-gyroscope';
+
 
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
@@ -33,11 +34,33 @@ function Box(props) {
 }
 
 export default function App() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [z, setZ] = useState(0);
+
+  setInterval(() => {
+    const gyroscope = getGyroscope();
+    setX(gyroscope.x);
+    setY(gyroscope.y);
+    setZ(gyroscope.z);
+  }, 1000);
+
+
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center', height: '100vh' }}>
-      <Canvas>
-        <Box position={[0.1, 0, 0]} />
-      </Canvas>
+    <div>
+      <ul>
+        <li>x: {x}</li>
+        <li>y: {y}</li>
+        <li>z: {z}</li>
+      </ul>
+      <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center', height: '100vh' }}>
+
+        <Canvas>
+          <Box position={[0.1, 0, 0]} />
+        </Canvas>
+      </div>
     </div>
+
   )
 }
