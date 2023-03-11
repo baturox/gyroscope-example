f0c03e78b883a74e28b32676641d767c42f1f3d1
Batuhan SAHIN <baturox@gmail.com>
2023-03-11 12:52:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import getGyroscope from 'reactjs-gyroscope';
 
-function App() {
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  useFrame((state, delta) => {
+    setInterval(() => {
+      const gyroscope = getGyroscope();
+      console.log(gyroscope.x);
+      console.log(ref);
+
+      ref.current.position.x = gyroscope.x / 90;
+      ref.current.position.y = gyroscope.y / 90;
+      ref.current.position.z = gyroscope.z / 90;
+    }, 1000);
+  })
+
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+
+  // Return the view, these are regular Threejs elements expressed in JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh
+      {...props}
+      ref={ref}
+      scale={1}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'orange'} />
+    </mesh>
+  )
 }
 
-export default App;
+export default function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center', height: '100vh' }}>
+      <Canvas>
+        <Box position={[0.1, 0, 0]} />
+      </Canvas>
+    </div>
+  )
+}
